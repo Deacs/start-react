7be592e67874f528e8117d7ce6a59cf38cc249ac
David <throwasix@gmail.com>
2018-08-02 10:07:44 +0100

    Axios pulling real user data

--- githubCards.js
+++ githubCards.js
@@ -12,25 +12,62 @@ const Card = (props) => {
   );
 };
 
-let data = [
-	{
-    name:"Deacs",
-    avatar_url: "https://avatars0.githubusercontent.com/u/1513341?v=4" ,
-    company: "Throwasix Inc."
-  },
-  {
-    name:"Paul O'Shanessy",
-    avatar_url: "https://avatars0.githubusercontent.com/u/8445?v=4",
-    company: "Facebook"
-  }
-];
-
 const CardList = (props) => {
 	return (
   	<div>
-  	 	{props.cards.map(card => <Card {...card} />)}
+  	 	{props.cards.map(card => <Card key={card.id} {...card} />)}
   	</div>
   );
 };
 
-ReactDOM.render(<CardList cards={data} />, mountNode);
+class Form extends React.Component {
+
+	state = { userName: '' };
+
+	handleSubmit = (event) => {
+  	event.preventDefault();
+    
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+    	.then(resp => {
+      	this.props.onSubmit(resp.data);
+      	this.setState({ userName: '' });
+      });
+    
+  };
+  
+  render() {
+  	return (
+    	<form onSubmit={this.handleSubmit}>
+          <input type="text" 
+					value={this.state.userName} 
+          onChange={(event) => {this.setState({ userName: event.target.value })}}
+          placeholder=""Github username/>
+        <button type="submit">Add card</button>
+    	</form>
+    );
+  };
+};
+
+class App extends React.Component {
+	
+  state = {
+    cards: []
+  };
+  
+  addNewCard = (cardInfo) => {
+    this.setState(prevState => ({
+      cards: prevState.cards.concat(cardInfo)
+    }));
+  };
+ 
+  render() {
+  	return (
+    	<div>
+    	  <Form onSubmit={this.addNewCard} />
+        <CardList cards={this.state.cards}/>
+    	</div>
+    ); 
+  };
+}; 
+
+ReactDOM.render(<App />, mountNode);
